8bd1d074167b45873ed4c215feb37dede006a881
Leonard <leonard.ladva@gmail.com>
2025-11-18 15:28:55 +0200

    Styled new habit form

--- src/components/Sidepanel/Screens/HabitNewScreen.tsx
+++ src/components/Sidepanel/Screens/HabitNewScreen.tsx
@@ -1,11 +1,27 @@
 import type { ScreenProps } from "./types";
+import "./HabitNewScreen.css";
 
 function HabitNewScreen(props: ScreenProps) {
   return (
     <>
       <h2>New Habit</h2>
-	  <button onClick={() => props.goTo("edit")}>Edit</button>
-	  <button onClick={() => props.goTo("list")}>List</button>
+      <button onClick={() => props.goTo("edit")}>Edit</button>
+      <button onClick={() => props.goTo("list")}>List</button>
+      <form action="" className="flex flex-col gap-1 w-4/5 m-auto items-start">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" placeholder="Habit Name"/>
+        <label htmlFor="">Type</label>
+        <select name="" id="" disabled>
+          <option value="daily">Daily</option>
+          <option value="Weekly">Weekly</option>
+          <option value="Monthly">Monthly</option>
+        </select>
+        <label htmlFor="color">Color</label>
+        <div className="input-box">
+          <input type="color" name="color" />
+        </div>
+        <button type="submit">Save</button>
+      </form>
     </>
   );
 }
