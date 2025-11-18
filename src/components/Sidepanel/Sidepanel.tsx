d30a812c91db0e1c83e239c255e1ced65fd224c3
Leonard <leonard.ladva@gmail.com>
2025-11-18 11:21:23 +0200

    Added tailwind.

--- src/components/Sidepanel/Sidepanel.tsx
+++ src/components/Sidepanel/Sidepanel.tsx
@@ -10,34 +10,48 @@ type SidepanelProps = {
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-
 function Sidepanel(props: PropsWithChildren<SidepanelProps>) {
   const [screen, setScreen] = useState<Screen>("list");
 
   const screens: Record<Screen, JSX.Element> = {
     list: <HabitsListScreen goTo={setScreen} />,
-    new: <HabitNewScreen goTo={setScreen}/>,
-    edit: <HabitsEditScreen goTo={setScreen}/>,
+    new: <HabitNewScreen goTo={setScreen} />,
+    edit: <HabitsEditScreen goTo={setScreen} />,
   };
 
-  if (!props.isOpen) return <></>;
-
   return (
-    <div className={"sidepanel" + (props.isOpen ? " open" : "")}>
-      <button
-        className="back-button"
-        onClick={() => {
-          if (screen == "list") {
-            props.setIsOpen(false);
-          } else {
-            setScreen("list");
-          }
-        }}
+    <>
+      {/* backdrop */}
+      <div
+        className={`fixed inset-0 bg-black/50 transition-opacity ${
+          props.isOpen
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
+        }`}
+        onClick={() => props.setIsOpen(false)}
+      />
+      {/* drawer panel */}
+      <div
+        className={`fixed right-0 top-0 h-full w-64 bg-white shadow-lg transition-transform duration-300 ${
+          props.isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
       >
-        Back
-      </button>
-      {screens[screen]}
-    </div>
+        {/* content */}
+        <button
+          className="back-button"
+          onClick={() => {
+            if (screen == "list") {
+              props.setIsOpen(false);
+            } else {
+              setScreen("list");
+            }
+          }}
+        >
+          Back
+        </button>
+        {screens[screen]}
+      </div>
+    </>
   );
 }
 
