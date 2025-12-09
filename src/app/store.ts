84231e963ebf63f86c69a6f891c961383f60b677
Leonard Ladva <leonard.ladva@gmail.com>
2025-12-09 11:47:22 +0200

    Implemented saving state to localStorage with Redux Persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,38 @@
 import type { Action, ThunkAction } from '@reduxjs/toolkit'
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import habitsReducer from '../features/habitsSlice'
 import sidepanelUiReducer from '../features/sidepanel/sidepanelUiSlice'
+import { persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
-export const store = configureStore({
-  reducer: {
+const rootReducer = combineReducers({
 	habits: habitsReducer,
 	sidepanel: sidepanelUiReducer,
-  },
-})
+});
+
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: ['habits'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+	reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+// export default () => {
+// 	return { store, persistor }
+// }
+
+// export const store = configureStore({
+//   reducer: {
+// 	habits: habitsReducer,
+// 	sidepanel: sidepanelUiReducer,
+//   },
+// })
 
 // Infer the type of `store`
 export type AppStore = typeof store
